50cacfed3e0c4b4645a06c8912ca43406d100f97
Anton AIG <66064832+AntonAIG@users.noreply.github.com>
2022-02-01 10:38:44 +0100

    sql_ront_2020_union

---
+++ sql_script/sql_ront_2020_union.sql
@@ -0,0 +1,61 @@
+SELECT 
+        timestamp, P_1, U_1_RMS
+
+    FROM proven-aura-332322.cvr_ront_2020.ront_01
+
+UNION ALL
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_02
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_03
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_04
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_05
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_06
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_07
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_08
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_09
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_10
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_11
+UNION ALL 
+    SELECT 
+        timestamp, P_1, U_1_RMS
+    FROM 
+        proven-aura-332322.cvr_ront_2020.ront_12
+
